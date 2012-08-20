6ab6d1472a76fc5d0b97a8921e181c258b4d5be3
Khue Vu <khuevu@gmail.com>
2012-08-21 00:03:49 +0800

    Initialize Blog

---
+++ README.md
@@ -0,0 +1,1 @@
+The code of my blog using [Pelican](http://pelican.notmyidea.org), a static site generator written in Python
---
+++ pelican.conf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+SITENAME = u"Water Spinach"
+AUTHOR = u"Vu Minh Khue"
+SITEURL = '/'
+
+TIMEZONE = 'Asia/Singapore'
+
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+         )
+
+# URL
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+AUTHOR_URL = 'about.html'
+AUTHOR_SAVE_AS = 'about.html'
+CATEGORY_URL = 'category/{name}.html'
+CATEGORY_SAVE_AS = 'category/{name}.html'
+TAG_URL = 'tag/{name}.html'
+TAG_SAVE_AS = 'tag/{name}.html'
+TAGS_SAVE_AS = 'tags.html'
+ARCHIVES_SAVE_AS = 'archive.html'
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DATE_FORMAT = {
+        'en': '%a, %d %b %Y'
+        }
+
+
+# PAGINATION
+DEFAULT_PAGINATION = 10 
+DEFAULT_ORPHANS = 1
+
+#SOCIAL PLUGIN
+TWITTER_USERNAME = 'khuevu'
